bb22ca5adac5c227206c49745b9db3fdff046d4f
imloama <imloama@gmail.com>
2021-06-07 15:17:54 +0800

    fix gen api

--- main.go
+++ main.go
@@ -1,5 +1,86 @@
 package gali
 
+import (
+	"github.com/gogf/gf-cli/library/allyes"
+	"github.com/gogf/gf-cli/library/mlog"
+	"github.com/gogf/gf/os/gcmd"
+	"github.com/gogf/gf/text/gstr"
+	"github.com/imloama/gf-api-cli/commands/gen"
+)
+
+
+var (
+	helpContent = gstr.TrimLeft(`
+USAGE
+    gf COMMAND [ARGUMENT] [OPTION]
+
+COMMAND
+    env        show current Golang environment variables
+    get        install or update GF to system in default...
+    gen        automatically generate go files for ORM models...
+    mod        extra features for go modules...
+    run        running go codes with hot-compiled-like feature...
+    init       create and initialize an empty GF project...
+    help       show more information about a specified command
+    pack       packing any file/directory to a resource file, or a go file...
+    build      cross-building go project for lots of platforms...
+    docker     create a docker image for current GF project...
+    swagger    swagger feature for current project...
+    update     update current gf binary to latest one (might need root/admin permission)
+    install    install gf binary to system (might need root/admin permission)
+    version    show current binary version info
+
+OPTION
+    -y         all yes for all command without prompt ask 
+    -?,-h      show this help or detail for specified command
+    -v,-i      show version information
+
+ADDITIONAL
+    Use 'gf help COMMAND' or 'gf COMMAND -h' for detail about a command, which has '...' 
+    in the tail of their comments.
+`)
+)
 func main()  {
-	
+	allyes.Init()
+	command := gcmd.GetArg(1)
+	// Help information
+	if gcmd.ContainsOpt("h") && command != "" {
+		help(command)
+		return
+	}
+	switch command {
+	case "help":
+		help(gcmd.GetArg(2))
+	case "version":
+		version()
+	case "gen":
+		gen.Run()
+	default:
+		for k := range gcmd.GetOptAll() {
+			switch k {
+			case "?", "h":
+				mlog.Print(helpContent)
+				return
+			case "i", "v":
+				version()
+				return
+			}
+		}
+		mlog.Print(helpContent)
+	}
+}
+
+// help shows more information for specified command.
+func help(command string) {
+	switch command {
+	case "gen":
+		gen.Help()
+	default:
+		mlog.Print(helpContent)
+	}
+}
+
+// version prints the version information of the cli tool.
+func version() {
+	mlog.Printf("gali version: 0.0.1")
 }
